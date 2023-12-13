f1b77e1d08b8bca1518d7a816cd534524bd09386
fa-ruz <fairouz.adlane@isfsc.be>
2023-12-13 10:53:33 +0100

    test

---
+++ repertoire-SetForMe-main/setforme/page-etirements.php
@@ -0,0 +1,136 @@
+<?php
+get_header(); ?>
+<div class="container mt-5">
+    
+<h2 class="video-title">5 minutes</h2>
+    <!-- Première série -->
+    <div class="row mb-4">
+        <div class="col-md-4">
+        <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?> /assets\e-1.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                    <h1>Strength Side</h1>
+                    <p class="card-text">Tout ce dont vous avez besoin, c'est de 5 minutes et d'un mur (ou d'un arbre). </p>
+                    <a href="https://www.youtube.com/watch?v=Jtf1_WdJuxw" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?> /assets\e-2.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>Mady Morisson</h1>
+                    <p class="card-text">C'est une excellente façon de commencer votre journée et de créer une routine matinale consciente mais active.</p>
+                    <a href="https://www.youtube.com/watch?v=sAf67xFS-qE" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?> /assets\e-3.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>Strength Side</h1>
+                    <p class="card-text">Un moyen rapide d’ouvrir votre corps. Étirez vos hanches, vos chevilles, vos épaules et votre colonne vertébrale et sentez-vous mieux en seulement 5 minutes.</p>
+                    <a href="https://www.youtube.com/watch?v=whf9pRHbH0o" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Deuxième série -->
+    <h2 class="video-title">10 minutes</h2>
+    <div class="row mb-4">
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?> /assets\e-4.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>Growingannanas</h1>
+                    <p class="card-text">Offrez-vous cet étirement relaxant de 10 minutes pour aider votre corps à récupérer. Nous aimons tous faire un bon étirement, n'est-ce pas ?! Faisons-le!</p>
+                    <a href="https://www.youtube.com/watch?v=biNUnu8INqg" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?> /assets\e-5.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>BullyJuice</h1>
+                    <p class="card-text">Augmentez la qualité de la récupération et prévenez de futures blessures avec cette routine de mouvement de 10 minutes !</p>
+                    <a href="https://www.youtube.com/watch?v=f-G70VJMZvU" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?> /assets\e-6.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>Daniela Suarez</h1>
+                    <p class="card-text">Ces exercices d’étirement pour les muscles raides sont une routine parfaite pour la maison !</p>
+                    <a href="https://www.youtube.com/watch?v=YfCK3uOz1r4" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Troisième série -->
+      <h2 class="video-title">15 minutes</h2>
+    <div class="row mb-4">
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?> /assets\e-7.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>Tom Merrick</h1>
+                    <p class="card-text">ACCESS ALL MY WORKOUTS: bodyweightwarrior.co.uk </p>
+                    <a href="https://www.youtube.com/watch?v=L_xrDAtykMI" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?> /assets\e-8.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>Growingannanas</h1>
+                    <p class="card-text">Allez-y doucement pour votre corps et votre esprit afin de vous sentir frais et rechargé - prêt pour une autre nouvelle semaine d'entraînement !</p>
+                    <a href="https://www.youtube.com/watch?v=aO1boUJhjvk" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?> /assets\e-9.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>Yoga With Tim</h1>
+                    <p class="card-text">Cette routine vous guidera à travers des exercices pour étirer et renforcer tout le corps. Entraînez-vous quotidiennement pour de meilleurs résultats.</p>
+                    <a href="https://www.youtube.com/watch?v=0oYnT5OBzKg" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
