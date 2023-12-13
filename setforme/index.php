e761f573dfe7d32344a9e805d439bd5e010cda45
linatdr <lina.tordeur@isfsc.be>
2023-12-13 10:39:50 +0100

    dossier mis dans wp

---
+++ setforme/index.php
@@ -0,0 +1,69 @@
+<?php
+get_header(); ?>
+<h1>TON CORPS,</h1>
+<h2>ton chemin.</h2>
+
+ 
+<h3>NOS CATEGORIES</h3>
+<p>Explorez nos catégories de sport :<br>
+Découvrez, choisissez, excellez.</p>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card mb-4 rounded-0 bg-black text-white" style="width: 728px height: 645px;">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets\Groupe 246.png" alt="test" class="img-fluid" width="727" height="432">
+    <div class="card-body d-flex flex-column">
+        <h4 class="card-title">HAUT DU CORPS</h4>
+        <a href="#" class="btn btn-primary mt-auto align-self-end">See Profile</a>
+    </div>
+            </div>
+
+            <div class="card mb-4 rounded-0 bg-black text-white" style="width: 728px height: 645px;">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets\Groupe 251.png" alt="test" class="img-fluid" width="727" height="432">
+    <div class="card-body d-flex flex-column">
+        <h4 class="card-title">TOUT LE CORPS</h4>
+        <a href="#" class="btn btn-primary mt-auto align-self-end">See Profile</a>
+    </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card mb-4 rounded-0 bg-black text-white" style="width: 728px height: 645px;">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets\Groupe 256.png" alt="test" class="img-fluid" width="727" height="432">
+    <div class="card-body d-flex flex-column">
+        <h4 class="card-title">BAS CORPS</h4>
+        <a href="#" class="btn btn-primary mt-auto align-self-end">See Profile</a>
+    </div>
+            </div>
+
+            <div class="card mb-4 rounded-0 bg-black text-white" style="width: 728px height: 645px;;">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets\Groupe 261.png" alt="test" class="img-fluid" width="727" height="432">
+    <div class="card-body d-flex flex-column">
+        <h4 class="card-title">ETIREMENTS</h4>
+        <a href="#" class="btn btn-primary mt-auto align-self-end">See Profile</a>
+    </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="p-3 mb-2 bg-black text-white"><blockquote class="blockquote"><h3>A PROPOS</h3><br><p>SetForMe révolutionne votre parcours sportif en vous offrant une expérience unique
+et personnalisée. Notre site propose une vaste gamme de vidéos adaptées à vos 
+besoins sportifs spécifiques. Comment ? Grâce à notre quizz intelligent qui vous 
+guide dans la personnalisation de votre programme. Répondez à quelques questions
+simples, et découvrez des vidéos conçues sur mesure pour vous. De la musculation 
+à la flexibilité, SetForMe s'ajuste à vos objectifs, vous offrant une expérience fitness 
+aussi unique que vous l'êtes. Rejoignez-nous dans cette aventure où chaque séance
+devient une étape vers votre meilleure version!</p></blockquote></div>
+
+
+        <div class="card mb-4 rounded-0 bg-black text-white" style="width: 573px; height: 446px;">
+    <div class="card-body d-flex flex-column">
+        <h4 class="card-title">HAUT DU CORPS</h4>
+        <a href="#" class="btn btn-primary mt-auto align-self-end">See Profile</a>
+        <i class="bi bi-eye-fill"></i>
+    </div>
+            </div>
+<?php get_footer(); ?>
---
+++ setforme/page-bas-du-corps.php
@@ -0,0 +1,138 @@
+
+<?php
+get_header(); ?>
+<div class="container mt-5">
+    
+<h2 class="video-title">5 minutes</h2>
+    <!-- Première série -->
+    <div class="row mb-4">
+        <div class="col-md-4">
+        <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/Rectangle 117.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                    <h1>Allblanc TV</h1>
+                    <p class="card-text">Il s’agit essentiellement d’une routine d’entraînement à domicile (aucun équipement requis) que vous pouvez faire partout, à la maison ou au gymnase.</p>
+                    <a href="https://youtu.be/1fjeQKI-5wg?si=thBEZ_Zh867e9is9" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/Rectangle 118.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>LivFitness</h1>
+                    <p class="card-text">Les entraînements courts peuvent-ils être efficaces ? Oui. Cinq minutes d’exercice à la fois peuvent être bénéfiques pour votre santé de différentes manières.</p>
+                    <a href="LIEN_VIDEO_2_SERIE_1" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/Rectangle 119.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>BullyJuice</h1>
+                    <p class="card-text">C'est l'entraînement à domicile parfait pour vos JAMBES! Pensez-vous pouvoir terminer cet entraînement de 5 minutes sans vous arrêter? INSANE!</p>
+                    <a href="LIEN_VIDEO_3_SERIE_1" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Deuxième série -->
+    <h2 class="video-title">10 minutes</h2>
+    <div class="row mb-4">
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/Rectangle 120.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>MadFit</h1>
+                    <p class="card-text">Essayez ce tueur à la maison LEG/BUTT/CUISSE entraînez-vous SANS ÉQUIPEMENT!</p>
+                    <a href="LIEN_VIDEO_1_SERIE_2" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/Rectangle 121.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>BullyJuice</h1>
+                    <p class="card-text">Envie d’avoir l’impression d’être retourné à la salle de sport ?</p>
+                    <a href="LIEN_VIDEO_2_SERIE_2" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/Rectangle 122.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>Train With Gainsbybrains</h1>
+                    <p class="card-text">10 Minutes d'exercices de jambes ciblant les fessiers/fessiers, quadriceps et ischio-jambiers ! Tout ce dont vous avez besoin est un haltère. </p>
+                    <a href="LIEN_VIDEO_3_SERIE_2" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Troisième série -->
+      <h2 class="video-title">15 minutes</h2>
+    <div class="row mb-4">
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/Rectangle 123.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>Juice & Toya</h1>
+                    <p class="card-text">Rejoignez-nous pour un entraînement d'haltères du bas du corps de 15 minutes qui dynamisera à coup sûr vos jambes!</p>
+                    <a href="LIEN_VIDEO_1_SERIE_3" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/Rectangle 124.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>MadFit</h1>
+                    <p class="card-text">C'est un entraînement INTENSE de 15 minutes! Croyez-moi, vous aurez mal après celui-ci!</p>
+                    <a href="LIEN_VIDEO_2_SERIE_3" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card custom-card">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/Rectangle 125.png" alt="test" class="card-img-top img-fluid">
+                <div class="card-body">
+                <h1>BullyJuice</h1>
+                    <p class="card-text">Faisons pousser ces jambes! Voici une séance d’entraînement à faire n’importe où et n’importe quand!</p>
+                    <a href="LIEN_VIDEO_3_SERIE_3" class="">
+                        <i class="bi bi-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
+
