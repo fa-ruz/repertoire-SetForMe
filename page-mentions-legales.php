914dd81e7eff1b2273960a21fb061c6d29179b48
linatdr <lina.tordeur@isfsc.be>
2023-12-14 17:23:37 +0100

    boucle php pour intégrer le contenu wp des pages
    + j'ai enlevé le titre des pages grace au css (petit détail)

--- page-mentions-legales.php
+++ page-mentions-legales.php
@@ -1,1 +1,14 @@
-<?php get_template_part('header3'); ?>
+<?php get_template_part('header3'); 
+while (have_posts()) :
+    the_post();
+    ?>
+    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <div class="entry-content">
+            <?php the_content(); ?>
+        </div>
+    </div>
+    <?php
+endwhile;
+
+get_footer();
