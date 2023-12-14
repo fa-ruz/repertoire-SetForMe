3cf758b6c06756056fbd00052ca3afad603ff3b5
fa-ruz <fairouz.adlane@isfsc.be>
2023-12-14 19:21:22 +0100

    page accueil front end

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 get_header(); ?>
 
 
-<img src="<?php echo get_template_directory_uri(); ?>/assets\images\accueil-formes.png" alt="test" class="img-fluid">
-
 <h1>TON CORPS,</h1>
 <h2>ton chemin.</h2>
 
@@ -16,7 +14,7 @@ Découvrez, choisissez, excellez.</p>
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <div class="card mb-4 rounded-0 bg-black text-white" style="width: 728px height: 645px;">
+            <div class="card mb-4 rounded-0 bg-black text-white">
             <img src="<?php echo get_template_directory_uri(); ?>/assets\Groupe 246.png" alt="test" class="img-fluid" width="727" height="432">
     <div class="card-body d-flex flex-column">
         <h4 class="card-title">HAUT DU CORPS</h4>
@@ -24,7 +22,7 @@ Découvrez, choisissez, excellez.</p>
     </div>
             </div>
 
-            <div class="card mb-4 rounded-0 bg-black text-white" style="width: 728px height: 645px;">
+            <div class="card mb-4 rounded-0 bg-black text-white">
             <img src="<?php echo get_template_directory_uri(); ?>/assets\Groupe 251.png" alt="test" class="img-fluid" width="727" height="432">
     <div class="card-body d-flex flex-column">
         <h4 class="card-title">TOUT LE CORPS</h4>
@@ -34,7 +32,7 @@ Découvrez, choisissez, excellez.</p>
         </div>
 
         <div class="col-md-6">
-            <div class="card mb-4 rounded-0 bg-black text-white" style="width: 728px height: 645px;">
+            <div class="card mb-4 rounded-0 bg-black text-white">
             <img src="<?php echo get_template_directory_uri(); ?>/assets\Groupe 256.png" alt="test" class="img-fluid" width="727" height="432">
     <div class="card-body d-flex flex-column">
         <h4 class="card-title">BAS CORPS</h4>
@@ -42,7 +40,7 @@ Découvrez, choisissez, excellez.</p>
     </div>
             </div>
 
-            <div class="card mb-4 rounded-0 bg-black text-white" style="width: 728px height: 645px;;">
+            <div class="card mb-4 rounded-0 bg-black text-white">
             <img src="<?php echo get_template_directory_uri(); ?>/assets\Groupe 261.png" alt="test" class="img-fluid" width="727" height="432">
     <div class="card-body d-flex flex-column">
         <h4 class="card-title">ETIREMENTS</h4>
@@ -62,14 +60,37 @@ simples, et découvrez des vidéos conçues sur mesure pour vous. De la musculat
 aussi unique que vous l'êtes. Rejoignez-nous dans cette aventure où chaque séance
 devient une étape vers votre meilleure version!</p></blockquote></div>
 
-
-        <div class="card mb-4 rounded-0 bg-black text-white" style="width: 573px; height: 446px;">
-    <div class="card-body d-flex flex-column">
-        <h4 class="card-title">HAUT DU CORPS</h4>
-        <a href="#" class="btn btn-primary mt-auto align-self-end">See Profile</a>
-        <i class="bi bi-eye-fill"></i>
-    </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="card-avis mb-3 rounded-0 bg-black text-white" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-8">
+            <div class="card-body">
+              <p class="card-text">SetForMe a vraiment changé la donne pour moi. Les vidéos personnalisées correspondent bien à mes besoins, et le quizz m'a aidé à cibler mes objectifs. J'adore la variété des exercices proposés.</p>
+              <p class="card-text mt-3"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6">
+      <div class="card-avis mb-3 rounded-0 bg-black text-white" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-8">
+            <div class="card-body">
+              <p class="card-text">SetForMe a vraiment changé la donne pour moi. Les vidéos personnalisées correspondent bien à mes besoins, et le quizz m'a aidé à cibler mes objectifs. J'adore la variété des exercices proposés.</p>
+              <p class="card-text mt-3"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 
            
 <?php get_footer(); ?>
