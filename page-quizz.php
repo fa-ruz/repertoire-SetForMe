aa5004b8162c9a3c5900e4527b2098af244af8e0
linatdr <lina.tordeur@isfsc.be>
2023-12-15 15:34:49 +0100

    page quizz

--- page-quizz.php
+++ page-quizz.php
@@ -1,25 +1,30 @@
-<?php get_header(); ?>
-<script>
-// Votre code JavaScript pour le quizz ici
-var quizData = {
-  question1: "Réponse 1",
-  question2: "Réponse 2",
-  // ... d'autres questions et réponses
-};
+<?php get_template_part('header3'); ?>
 
-var xhr = new XMLHttpRequest();
-xhr.open('POST', 'process_quizz.php', true);
-xhr.setRequestHeader('Content-Type', 'application/json');
+<body>
+  <div class="wrapper">
+    <div class="quiz-container d-flex flex-column justify-content-center align-items-center mt-5" style="min-height: 70vh;">
+      <div class="head-quiz">
+      <button type="button" id="Retour" class="btn-retour btn-dark">Retour</button>
+        <h2 class="quiz-title">Q1</h2>
+      </div>
+      <div class="quiz-body">
+        <h2 class="quiz-question" id="question">Pour commencer, quelle partie spécifique de votre corps souhaitez-vous cibler et travailler avec nos vidéos ?</h2>
+        <div class="row quiz-option">
+          <div class="col-md-6 text-center">
+            <li>Haut du corps</li>
+          </div>
+          
+          <div class="col-md-6 text-center">
+            <li>Bas du corps</li>
+          </div>
+          <div class="col-md-6 text-center">
+            <li>Tout le corps</li>
+          </div>
+        </div>
+        <div id="resultats"></div>
+      </div>
+    </div>
+  </div>
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    // Gérer la réponse du serveur si nécessaire
-    console.log(xhr.responseText);
-  }
-};
+  <!-- Le reste de votre code
 
-xhr.send(JSON.stringify(quizData));
-</script>
-
-</body>
-</html>
