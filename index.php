b9691849d03294cd74900d1fe733d759f8de247f
fa-ruz <fairouz.adlane@isfsc.be>
2023-12-19 12:45:43 +0100

    test

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ add_action('wp_enqueue_scripts', 'enqueue_slick_styles');
 
 ?>
 
-
 <div id="content-container">
 <section class="background-section">
     <!-- Contenu de la section -->
@@ -37,23 +36,20 @@ add_action('wp_enqueue_scripts', 'enqueue_slick_styles');
         if ($categories_query->have_posts()) :
             while ($categories_query->have_posts()) : $categories_query->the_post();
         ?>
-<div class="col-md-6 mb-4">
-    <div class="card rounded-0 bg-black text-white">
-        <?php if (has_post_thumbnail()) : ?>
-            <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="<?php the_title(); ?>" class="img-fluid">
-        <?php endif; ?>
-        <div class="card-body d-flex flex-column">
-            <h2 class="card-title"><?php the_title(); ?>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-flèche.png" alt="flèche" class="img-fluid arrow-icon ml-auto" alt="icone">
-            </h2>
-            <a href="<?php the_permalink(); ?>" class="btn1 mt-2 align-self-end" id="monBtn"></a>
-        </div>
-    </div>
-    <!-- Ajout d'un paragraphe en dessous de chaque carte -->
-    <p class="card-description mt-2"><?php the_excerpt(); ?></p>
-</div>
-
-
+                <div class="col-md-6">
+                    <div class="card mb-4 rounded-0 bg-black text-white" >
+                        <?php if (has_post_thumbnail()) : ?>
+                            <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="<?php the_title(); ?>" class="img-fluid" width="728" height="533">
+                        <?php endif; ?>
+                        <div class="card-body d-flex flex-column">
+                            <h4 class="card-title" ><?php the_title(); ?></h4>
+                            <a href="<?php the_permalink(); ?>" class="btn1 mt-auto align-self-end" id="monBtn">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-flèche.png" alt="flèche" class="img-fluid" alt="icone" style="position:relative; z-index:20; margin:0px;">
+                                <span style="margin-left:10px;"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
         <?php
             endwhile;
             wp_reset_postdata(); // Réinitialiser les données de la requête
@@ -98,63 +94,49 @@ wp_reset_postdata(); // Rétablir les données de publication originales
 <h3>RÉSULTATS</h3>
 <p>Des réussites, des changements, des vies transformées.<br>Explorez les témoignages SetForMe et laissez-vous inspirer</p>
 
-<div class="carousel-images">
-                <div class="images">
-                    <img src="img/vert.jpg" alt="Image 1">
-                    <img src="img/orange.jpg" alt="Image 2">
-                    <img src="img/blou.jpg" alt="Image 3">
-                    <img src="img/mauve.jpg" alt="Image 4">
-                    <img src="img/blanc.jpg" alt="Image 5">
-                    <img src="img/rouge.jpg" alt="Image 6">
-                    <img src="img/rouge.jpg" alt="Image 7">
-                </div>
-            </div>
-            <div class="nav-buttons">
-                <button class="prev"><</button>
-                <button class="next">></button>
-            </div>
-        </div>
-        
-      </main>
+<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image1.jpg" class="d-block w-100" alt="Image 1">
+      <div class="carousel-caption text-light"> <!-- Ajout de la classe text-light pour assurer une bonne visibilité du texte -->
+        <img src="profile1.jpg" alt="Profile Picture" class="rounded-circle">
+        <h5>Username 1</h5>
+        <p>Texte de l'avis 1</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="image2.jpg" class="d-block w-100" alt="Image 2">
+      <div class="carousel-caption text-light">
+        <img src="profile2.jpg" alt="Profile Picture" class="rounded-circle">
+        <h5>Username 2</h5>
+        <p>Texte de l'avis 2</p>
+      </div>
+    </div>
+    <!-- Ajoutez d'autres éléments de carousel selon vos besoins -->
+  </div>
 
-        <script>
-            document.addEventListener("DOMContentLoaded", function () {
-              const imagesContainer = document.querySelector(".carousel-images .images");
-              const prevButton = document.querySelector(".prev");
-              const nextButton = document.querySelector(".next");
-              const imageWidth = imagesContainer.clientWidth; // Largeur d'une image
-              const totalImages = imagesContainer.childElementCount; // Nombre total d'images
-              let currentIndex = 0; // Indice de l'image actuelle
-          
-              function updateButtonsState() {
-                prevButton.disabled = currentIndex === 0; // Désactiver le bouton "Précédent" à la première image
-                nextButton.disabled = currentIndex === totalImages - 4; // Désactiver le bouton "Suivant" lorsqu'il reste 4 images pour afficher
-              }
-          
-              nextButton.addEventListener("click", function () {
-                if (currentIndex < totalImages - 4) {
-                  currentIndex++;
-                  const offset = -currentIndex * imageWidth;
-                  imagesContainer.style.transform = `translateX(${offset}px)`;
-                  updateButtonsState();
-                }
-              });
-          
-              prevButton.addEventListener("click", function () {
-                if (currentIndex > 0) {
-                  currentIndex--;
-                  const offset = -currentIndex * imageWidth;
-                  imagesContainer.style.transform = `translateX(${offset}px)`;
-                  updateButtonsState();
-                }
-              });
-          
-              // Mettez à jour l'état des boutons au chargement de la page
-              updateButtonsState();
-            });
-          </script>
-
+  <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
 <?php get_footer(); ?>
 
+<script>
+  jQuery(document).ready(function ($) {
+    $('#myCarousel').carousel({
+        interval: 5000, // Réglez l'intervalle en millisecondes (facultatif)
+        pause: 'hover'
+    });
 
+    $('.carousel-control-prev, .carousel-control-next').css('color', 'black');
+});
+
+</script>
+</div>
 
