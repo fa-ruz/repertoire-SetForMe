d1517ef607a1c63b83aad5c8ec87055079e0b3cf
fa-ruz <fairouz.adlane@isfsc.be>
2023-12-21 15:03:55 +0100

    dimensions rectifiées

--- page-profil.php
+++ page-profil.php
@@ -15,20 +15,20 @@
 
 <div class="p-titre">
     <h2 style="margin-left: 50px;">Finie</h2>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="571" height="306" style="margin-left: 50px; margin-bottom: 50px;">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="470" height="260" style="margin-left: 50px; margin-bottom: 50px;">
 </div>
 
 
 <div class="p-titre">
     <h2 style="margin-left: 50px;">En cours</h2>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="571" height="306" style="margin-left: 50px; margin-bottom: 50px;">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="470" height="260" style="margin-left: 50px; margin-bottom: 50px;">
 </div>
 
 <h2 style="margin-left: 50px;">A faire</h2>
 <div class="p-titre" style="display: flex; margin-bottom: 20px;">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="571" height="306" style="margin-left: 50px; margin-right: 10px; margin-bottom: 50px;">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="571" height="306" style="margin-left: 30px; margin-right: 10px; margin-bottom: 50px;">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="571" height="306" style="margin-left: 30px; margin-bottom: 50px;">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="470" height="260" style="margin-left: 50px; margin-right: 10px; margin-bottom: 50px;">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="470" height="260" style="margin-left: 30px; margin-right: 10px; margin-bottom: 50px;">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hdc-1.png" alt="test" width="470" height="260" style="margin-left: 30px; margin-bottom: 50px;">
 </div>
 
 
