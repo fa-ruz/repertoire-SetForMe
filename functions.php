d1a1c830dc1db9e90ee954ec6e323786a7169ca0
linatdr <lina.tordeur@isfsc.be>
2023-12-21 12:38:41 +0100

    changement css pour wordpress en ligne

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ add_theme_support( 'menus' );
 function wpbootstrap_styles_scripts() {
     wp_enqueue_style('style', get_stylesheet_uri());
     wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
-    wp_enqueue_style('mainStyle', get_template_directory_uri() . '/assets/css/style.css');
+    wp_enqueue_style('mainStyle', get_template_directory_uri() . '/assets/css/app.css');
 
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap-bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js', false, '1.0.0', true);
